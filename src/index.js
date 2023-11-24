a2eeb51eb34e7f334fae03af7f1dbfdca58eca33
BigFatDuck1 <clementho999@gmail.com>
2023-11-24 19:21:22 +0800

    npx webpack executed which generated a main.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import _ from "lodash";
+
 function component() {
     const element = document.createElement("div");
 
-    // Lodash, currently included via a script, is required for this line to work
-    //See <script> in html
+    //// Lodash, currently included via a script, is required for this line to work
+    ////See <script> in html
     element.innerHTML = _.join(["Hello", "webpack"], " ");
 
     return element;
