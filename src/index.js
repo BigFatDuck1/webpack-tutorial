ba1ad3d041e1d83f9eda14cf37052d7630eb00a7
BigFatDuck1 <clementho999@gmail.com>
2023-11-26 00:10:49 +0800

    Setup for asset management tutorial

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import myName from "./myName.js";
 
 function component() {
     const element = document.createElement("div");
@@ -6,6 +7,8 @@ function component() {
     //// Lodash, currently included via a script, is required for this line to work
     ////See <script> in html
     element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+    element.textContent = myName("John Doe");
 
     return element;
 }
